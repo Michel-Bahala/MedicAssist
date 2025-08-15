f20871cddcc8e48ad606c143ca2d86c2a6144080
Michel Buroko Bahala <burokobahalokwibalmichel@gmail.com>
2025-08-15 14:19:34 +0000

    Remplace tous les éléments <path> qui ont un attribut "d" invalide ou in

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -14,23 +14,14 @@ const StethoscopeIcon: FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
     {...props}
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
     fill="none"
+    viewBox="0 0 24 24"
     stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
   >
-    <path d="M4 14a2 2 0 1 0 4 0V6a2 2 0 1 0-4 0v8Z" />
-    <path d="M6 4V2" />
-    <path d="M6 20v-4" />
-    <path d="M12 4h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4" />
-    <path d="M14 10V8" />
-    <path d="M14 12v8a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2Z" />
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14c3.866 0 7 3.134 7 7H5c0-3.866 3.134-7 7-7zm0-2a5 5 0 100-10 5 5 0 000 10z" />
   </svg>
 );
+
 
 export function Header() {
   const { setLanguage, t } = useTranslation();
